58b71e79f20156a7e18b70893002e1f75e3f5dba
Alexander Mazur <iamdinarium@gmail.com>
2025-04-26 19:13:40 +0300

    Стилизует блок туров

--- source/js/swiper.js
+++ source/js/swiper.js
@@ -24,3 +24,23 @@ const heroSwiper = new Swiper('.hero__swiper', {
     },
   }
 });
+
+const toursSwiper = new Swiper('.tours__swiper', {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  modules: [Navigation],
+  navigation: {
+    nextEl: '.tours__button--next',
+    prevEl: '.tours__button--prev',
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 18,
+    },
+    1440: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  }
+});
